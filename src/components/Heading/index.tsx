7b894f426b4cc06db27de978534664cd41a15c49
Denilson <denilsonraimundo17@gmail.com>
2021-03-14 11:01:20 -0300

    :art: Add style to Heading component

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -3,10 +3,19 @@ import * as S from './styles'
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
+  lineBottom?: boolean
+  lineLeft?: boolean
 }
 
-const Heading = ({ children, color = 'white' }: HeadingProps) => (
-  <S.Wrapper color={color}>{children}</S.Wrapper>
+const Heading = ({
+  children,
+  color = 'black',
+  lineBottom = false,
+  lineLeft = false
+}: HeadingProps) => (
+  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+    {children}
+  </S.Wrapper>
 )
 
 export default Heading
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,14 +1,39 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
+
 import { HeadingProps } from '.'
 
+const wrapperModifiers = {
+  lineLeft: (theme: DefaultTheme) => css`
+    border-left: 0.7rem solid ${theme.colors.secondary};
+    padding-left: ${theme.spacings.xxsmall};
+  `,
+  lineBottom: (theme: DefaultTheme) => css`
+    margin-bottom: ${theme.spacings.medium};
+    position: relative;
+
+    &::after {
+      border: 0.4rem solid ${theme.colors.primary};
+      bottom: -1rem;
+      content: '';
+      left: 0;
+      position: absolute;
+      width: 5rem;
+    }
+  `
+}
+
 export const Wrapper = styled.h2<HeadingProps>`
-  ${({ theme, color }) => css`
+  ${({ theme, color, lineBottom, lineLeft }) => css`
     color: ${theme.colors[color!]};
     font-size: ${theme.font.sizes.xlarge};
 
     ${media.greaterThan('medium')`
       font-size: ${theme.font.sizes.xxlarge};
     `}
+
+    ${lineBottom && wrapperModifiers.lineBottom(theme)}
+
+    ${lineLeft && wrapperModifiers.lineLeft(theme)}
   `}
 `
