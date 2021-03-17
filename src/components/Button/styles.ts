1181647d2d94b7589bab6c114241e68b2350b966
Denilson <denilsonraimundo17@gmail.com>
2021-03-17 07:22:49 -0300

    :sparkles: Init Button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,34 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+
+import { ButtonProps } from '.'
+
+type WrapperProps = Pick<ButtonProps, 'size'>
+
+const wrapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.xsmall};
+    height: 3rem;
+  `,
+  medium: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.small};
+    height: 4rem;
+    padding: ${theme.spacings.xxsmall} ${theme.font.sizes.medium};
+  `,
+  large: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.medium};
+    height: 5rem;
+    padding: ${theme.spacings.xxsmall} ${theme.font.sizes.xlarge};
+  `
+}
+
+export const Wrapper = styled.button<WrapperProps>`
+  ${({ theme, size }) => css`
+    background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
+    border: 0;
+    border-radius: ${theme.border.radius};
+    color: ${theme.colors.white};
+    padding: ${theme.spacings.xxsmall};
+
+    ${!!size && wrapperModifiers[size](theme)}
+  `}
+`
