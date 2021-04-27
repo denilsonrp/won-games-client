2a3eeefb35d1e310f5821be27587af72f9aea910
Denilson <denilsonraimundo17@gmail.com>
2021-04-26 21:08:12 -0300

    :sparkles: Init Menu component with mobile version

---
+++ src/components/Menu/styles.ts
@@ -0,0 +1,37 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const IconWrapper = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    cursor: pointer;
+    height: 2.4rem;
+    width: 2.4rem;
+  `}
+`
+
+export const LogoWrapper = styled.div`
+  ${media.lessThan('medium')`
+    left: 50%;
+    position: absolute;
+    transform: translateX(-50%);
+  `}
+`
+
+export const MenuGroup = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-grow: 1;
+    gap: ${theme.spacings.xsmall};
+    justify-content: flex-end;
+  `}
+`
+
+export const Wrapper = styled.menu`
+  ${({ theme }) => css`
+    align-items: center;
+    display: flex;
+    padding: ${theme.spacings.small} 0;
+    position: relative;
+  `}
+`
