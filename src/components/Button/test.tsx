624841b5d5ae23a1dc809fe96544d93091f95233
Denilson <denilsonraimundo17@gmail.com>
2021-04-29 22:44:15 -0300

    :sparkles: Init Ribbon component

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -54,13 +54,11 @@ describe('<Button />', () => {
   })
 
   it('should render Button as a link', () => {
-    const { debug, container } = renderWithTheme(
+    renderWithTheme(
       <Button as="a" href="/">
         Buy now
       </Button>
     )
-
-    debug(container)
 
     expect(screen.getByRole('link', { name: /Buy now/i })).toHaveAttribute(
       'href',
---
+++ src/components/Ribbon/stories.tsx
@@ -0,0 +1,28 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Ribbon, { RibbonProps } from '.'
+
+export default {
+  title: 'Ribbon',
+  component: Ribbon,
+  args: {
+    children: 'Best Sellers'
+  },
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+} as Meta
+
+export const Default: Story<RibbonProps> = (args) => (
+  <div
+    style={{
+      backgroundColor: '#888',
+      height: '20rem',
+      position: 'relative',
+      width: '20rem'
+    }}
+  >
+    <Ribbon {...args} />
+  </div>
+)
